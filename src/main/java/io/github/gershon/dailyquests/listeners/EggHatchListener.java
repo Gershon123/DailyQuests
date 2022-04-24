1bb5355b01276e8d0ee7fa4ee1b799823452240b
Gershon <test@test>
2022-04-24 12:57:48 -0700

    Update for 8.4

--- src/main/java/io/github/gershon/dailyquests/listeners/EggHatchListener.java
+++ src/main/java/io/github/gershon/dailyquests/listeners/EggHatchListener.java
@@ -1,6 +1,7 @@
 package io.github.gershon.dailyquests.listeners;
 
 import com.pixelmonmod.pixelmon.api.events.EggHatchEvent;
+import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
 import io.github.gershon.dailyquests.quests.Quest;
 import io.github.gershon.dailyquests.quests.tasks.impl.EggHatchTask;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -11,11 +12,15 @@ import java.util.List;
 public class EggHatchListener extends BaseListener {
 
     @SubscribeEvent
-    public void onEggHatch(EggHatchEvent event) {
-        Player player = (Player) event.pokemon.getOwnerPlayer();
+    public void onEggHatch(EggHatchEvent.Post event) {
+        Pokemon pokemon = event.getPokemon();
+        if (pokemon == null) {
+            return;
+        }
+        Player player = (Player) pokemon.getOwnerPlayer();
         List<Quest> quests = getQuestsForPlayer(player);
         if (quests != null && quests.size() > 0) {
-            List<Quest> captureQuests = EggHatchTask.getApplicableQuests(quests, event.pokemon.getSpecies());
+            List<Quest> captureQuests = EggHatchTask.getApplicableQuests(quests, pokemon.getSpecies());
             updateQuestsForPlayer(player, captureQuests, 1);
         }
     }
